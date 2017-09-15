2154f5abfffbaad6768f9788e6678eddda48f598
rera <david@rerainc.com>
2017-09-14 23:04:32 -0400

    fix grayscale dynamic map

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,6 +1,7 @@
 window.loadMap = function() {
   L.mapbox.accessToken = 'pk.eyJ1IjoiY2FibGUzNjM2IiwiYSI6Ijc0Mjk0ZTE5YTY1ZmMwMWJhNWVkMTNmMDcyYjI5MWJiIn0.2wRx10DFsGguR6QPkcUhDQ';
   window.map = L.mapbox.map('map', 'mapbox.streets').setView([26.7153, -80.0534], 11);
+  L.mapbox.styleLayer('mapbox://styles/mapbox/light-v9').addTo(map);
   window.map.scrollWheelZoom.disable();
 };
 
